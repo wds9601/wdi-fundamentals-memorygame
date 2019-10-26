73073894f47a824aed02e6d2d56cc9ef040d6b95
William Downey-Schell <Will@Williams-MacBook-Air.local>
2019-10-26 12:17:06 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped" + " " + cardOne);
+console.log("User flipped" + " " + cardThree);
