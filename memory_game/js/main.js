53347823fcc4e03d441571fcf917fb6bad8f4f49
William Downey-Schell <Will@Williams-MacBook-Air.local>
2019-10-27 13:34:28 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardThree);
+const cards = ["queen", "queen", "king", "king"];
+
+const cardOne = cards[0];
+const cardTwo = cards[2];
+
+const cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) { 
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+
+	console.log("User flipped" + " " + cardOne);
+	console.log("User flipped" + " " + cardTwo);
+}
