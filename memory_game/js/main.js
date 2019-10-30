09ca6cbeac7d75d0c9431d22133743e9ef085a4c
William Downey-Schell <Will@Williams-MacBook-Air.local>
2019-10-29 19:44:30 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,17 +6,27 @@ const cardTwo = cards[2];
 
 const cardsInPlay = [];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) { 
-
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
-
-	console.log("User flipped" + " " + cardOne);
-	console.log("User flipped" + " " + cardTwo);
 }
+
+function flipCard(cardId) {
+
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+flipCard(0);
+flipCard(2);
+
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
